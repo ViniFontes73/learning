36469e90bee33057ee5a7a8fa65b058a77ba587d
Vinicius <viniciusfontesads@gmail.com>
2023-07-25 09:58:25 -0300

    Segunda fornada

--- index.js
+++
@@ -1,8 +0,0 @@
-// do while loop - ele permite refazer algo fazendo algo antes.
-
-let nickname;
-
-do{
-    nickname = window.prompt( "Insira seu NickName:");
-} while(nickname == '')
-console.log("Olá", nickname)
